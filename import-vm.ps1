19fb5b70a63a9e993885baedd9385a2348843816
effko32 <78663210+effko32@users.noreply.github.com>
2021-11-24 18:13:08 +0400

    fix get-child

--- import-vm.ps1
+++ import-vm.ps1
@@ -1,4 +1,3 @@
 $VMName = "windows"
-
-dir "D:\packer\$VMName\Virtual Machines\*.vmcx" | Import-VM -Copy -VhdDestinationPath "D:\VirtualMachines\$VMName\Virtual Hard Disks" -VirtualMachinePath "D:\VirtualMachines\$VMName" -GenerateNewId
+Get-ChildItem "D:\packer\$VMName\Virtual Machines\*.vmcx" | Import-VM -Copy -VhdDestinationPath "D:\VirtualMachines\$VMName\Virtual Hard Disks" -VirtualMachinePath "D:\VirtualMachines\$VMName" -GenerateNewId
 Start-VM $VMName
