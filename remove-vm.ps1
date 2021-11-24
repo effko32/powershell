4a2c86548142af6bf9d2fdaf7717ae46c28cba7e
andrey.deputatov <andrey.deputatov@norbit.ru>
2021-11-24 22:49:28 +0400

    fix warning stopped vm

--- remove-vm.ps1
+++ remove-vm.ps1
@@ -1,4 +1,5 @@
 $VMName = "windows"
 Stop-VM $VMName -Force
+$WarningPreference = 'SilentlyContinue' # If VM already stopped
 Remove-VM $VMName -Force
 Remove-Item -Path "C:\VirtualMachines\$VMName" -Recurse
